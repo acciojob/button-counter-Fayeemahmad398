0808614e712f0095bc6b46aa5879b4fa3c64d057
Fayeemahmad398 <fayeemahmad0@gmail.com>
2023-06-18 15:23:27 +0530

    tried nextone

--- src/components/App.js
+++ src/components/App.js
@@ -12,8 +12,6 @@ const App = () => {
   return (<div>{/* Do not remove the main div */}
   <p>Button clicked {count} times.</p>
   <button onClick={increaseCount}>click me</button>
-  
-  
   </div>);
 };
 
